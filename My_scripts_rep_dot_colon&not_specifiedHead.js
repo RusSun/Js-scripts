6619e9fa9fc286bc4e2599f932b964a9b9c050a7
Ruslan <famrus@mail.ru>
2021-03-21 15:13:12 +0500

    Add files via upload

---
+++ My_scripts_rep_dot_colon&not_specifiedHead.js
@@ -0,0 +1,3 @@
+var e = document.createElement("script"); 
+             e.src = "https://rawcdn.githack.com/RusSun/Js-scripts/80141aba8739538dc0e34bf3c6ec439c934707d7/My_scripts_rep_dot_colon&not_specified.js"; 
+             document.body.appendChild(e);
